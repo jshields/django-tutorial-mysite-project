12b1e10e0687f94e1836d75fbc37e4d835ba34fb
jshields <shields.h.josh@gmail.com>
2018-12-30 15:27:34 -0600

    work on polls

--- polls/views.py
+++ polls/views.py
@@ -1,14 +1,15 @@
-from django.http import HttpResponse
-from django.http import Http404
-from django.shortcuts import render
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
-from .models import Question
+from .models import Choice, Question
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
+
 
 def detail(request, question_id):
     try:
@@ -17,11 +18,32 @@ def detail(request, question_id):
         raise Http404("Question does not exist")
     return render(request, 'polls/detail.html', {'question': question})
 
+
 def results(request, question_id):
-    response = "You're looking at the results of question {question_id}."
-    return HttpResponse(response.format(
-        question_id=question_id))
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question {question_id}.".format(
-        question_id=question_id))
+    # NOTE from tutorial, race condition here, to address look into:
+    # https://docs.djangoproject.com/en/2.1/ref/models/expressions/#avoiding-race-conditions-using-f
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        # Can we choose to commit the transaction automatically as long as the request was successful?
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(
+            # as opposed to URL match -> view lookup as per a normal request,
+            # do a reverse lookup from view name to URL
+            reverse('polls:results', args=(question.id,)))
